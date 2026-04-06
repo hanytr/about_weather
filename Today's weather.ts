e77c2ecd77d5b97069d0cbacd0eb5e0e903106bf
Hany Tran <hangtr.work@gmail.com>
2026-04-06 20:42:04 +0700

    today's weather

---
+++ Today's weather.ts
@@ -0,0 +1,8 @@
+let temperature = 37;          
+let conditions = "sunny"; 
+let sunriseTime = "6:00 AM";
+let sunsetTime = "6:00 PM";
+
+let sentence = `It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`;
+
+console.log(sentence);
