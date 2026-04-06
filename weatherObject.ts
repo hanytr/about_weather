f0e9b6950247e1fa3b6dad00132cb1e08b599a76
Hany Tran <hangtr.work@gmail.com>
2026-04-06 23:34:56 +0700

    weatherobject

---
+++ weatherObject.ts
@@ -0,0 +1,7 @@
+var weather = {
+    temperature: 37,
+    conditions: "sunny"
+};
+
+console.log(weather.temperature);
+console.log(`It's ${weather.temperature} degrees F and ${weather.conditions}.`);
